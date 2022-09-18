9e0ddf4e712b3fb4001843650ae8bfb522513341
girnal82 <grinal82@gmail.com>
2022-09-18 21:56:45 +0300

    initial commit

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,41 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if form.cleaned_data['is_main']:
+                count += 1
+                form.cleaned_data
+        if count > 1:
+            raise ValidationError('Основной раздел должен быть один!')
+
+        if count < 1:
+            raise ValidationError('Выберете один основной раздел!')
+        return super().clean()
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 0
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeInline]
+    list_display = ('id', 'text', 'published_at', 'image')
+
+    
+@admin.register(Tag)
+class TagsAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'name')
+
+    
+    
+    
+
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -11,6 +11,33 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Категория')
+    article = models.ManyToManyField(Article, related_name='tags', through='Scope')
+    
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Scope(models.Model):
+    id = models.AutoField(primary_key=True)
+    article = models.ForeignKey(Article, related_name='scope', on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    is_main = models.BooleanField(verbose_name='Основной')
+    
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+    
+
+
+
