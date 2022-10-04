e438f3aa870f6478c336e3b319efc8ac5db1b446
girnal82 <grinal82@gmail.com>
2022-10-04 16:23:32 +0300

    serializer, filters and permissions added to Viewset

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,15 +1,25 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+
+from advertisements.models import Advertisement
+from advertisements.permissions import IsOwnerOrReadOnly
+from advertisements.serializers import AdvertisementSerializer
+from advertisements.filters import AdvertisementFilter
 
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
-
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
+    
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['title', 'description',]                # Фильтрация по наименованию и описанию
+    filterset_class = AdvertisementFilter                    # Кастомный фильтр на дату и создателя
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated()]
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
         return []
