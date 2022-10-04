7b101065f1670ba66c1846afbcdef456aa53b1dd
girnal82 <grinal82@gmail.com>
2022-10-04 16:18:52 +0300

    added custom AdvertisementFilter for created_at and creator

--- 3.3-permissions/api_with_restrictions/advertisements/filters.py
+++ 3.3-permissions/api_with_restrictions/advertisements/filters.py
@@ -1,12 +1,12 @@
 from django_filters import rest_framework as filters
-
+from django_filters.rest_framework import DateFromToRangeFilter
 from advertisements.models import Advertisement
 
 
 class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
 
-    # TODO: задайте требуемые фильтры
-
+    created_at = DateFromToRangeFilter(field_name='created_at', lookup_expr='lt')
     class Meta:
         model = Advertisement
+        fields = ['created_at', 'creator',]
